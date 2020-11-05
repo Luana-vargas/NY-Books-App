bae2a93035dbbccc8432e36ab2357ac780e01312
luana-vargas <vargas.luana@outlook.com.br>
2020-11-05 14:08:16 -0300

    configurated toolbar

--- app/src/main/java/br/com/luana/nybooksmvvm/presentation/books/BooksActivity.kt
+++ app/src/main/java/br/com/luana/nybooksmvvm/presentation/books/BooksActivity.kt
@@ -3,22 +3,21 @@ package br.com.luana.nybooksmvvm.presentation.books
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.luana.nybooksmvvm.R
-import br.com.luana.nybooksmvvm.data.model.Book
+import br.com.luana.nybooksmvvm.presentation.base.BaseActivity
 import br.com.luana.nybooksmvvm.presentation.details.BookDetailsActivity
 import kotlinx.android.synthetic.main.activity_books.*
+import kotlinx.android.synthetic.main.include_toolbar.*
 
-class BooksActivity : AppCompatActivity() {
+class BooksActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_books)
 
-        toolbarMain.title = getString(R.string.books_title)
-        setSupportActionBar(toolbarMain)
+        setupToolbar(toolbarMain, R.string.books_title)
 
         val viewModel: BooksViewModel = ViewModelProviders.of(this).get(BooksViewModel::class.java)
         viewModel.getBooks()
--- app/src/main/java/br/com/luana/nybooksmvvm/presentation/details/BookDetailsActivity.kt
+++ app/src/main/java/br/com/luana/nybooksmvvm/presentation/details/BookDetailsActivity.kt
@@ -5,12 +5,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.luana.nybooksmvvm.R
+import br.com.luana.nybooksmvvm.presentation.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_book_details.*
+import kotlinx.android.synthetic.main.include_toolbar.*
 
-class BookDetailsActivity : AppCompatActivity() {
+class BookDetailsActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_details)
+
+        setupToolbar(toolbarMain, R.string.book_details_title)
 
         bookDetailsTitle.text = intent.getStringExtra(EXTRA_TITLE)
         bookDetailsDescription.text = intent.getStringExtra(EXTRA_DESCRIPTION)
